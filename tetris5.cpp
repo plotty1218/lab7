7f6983d013d776603bb4a25883561fd9b8016c4b
plotty1218 <plotty242424@gmail.com>
2016-05-29 16:58:37 +0800

    finish lab7

---
+++ tetris5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "genMino.h"
+using namespace std;
+int main(){
+	Mino * mino_ptr;
+
+	srandom(time(NULL));
+	for(int i=0;i<5;++i){
+		mino_ptr = genMino();
+		mino_ptr->paint();
+		delete mino_ptr;
+	}
+	return 0;
+}
